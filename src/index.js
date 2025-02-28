a7f77e45bc28aa9e4dee29dbfccdb98e5368458a
rakeshkota19 <rakeshkota19@gmail.com>
2025-02-28 13:22:04 +1100

    Minor import fix

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from "./App";
 import { Authenticator } from "@aws-amplify/ui-react";
 
 import { Amplify } from "aws-amplify";
-// import outputs from "./amplify_outputs.json";
-import outputs from "../amplify_outputs.json";
+import outputs from "./amplify_outputs.json";
+// import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
 const root = ReactDOM.createRoot(document.getElementById("root"));
